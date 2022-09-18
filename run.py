152a9859901079bdfa7efff2d540f3ea5edac205
Airosa <airosa32@gmail.com>
2022-09-18 14:48:05 -0300

    manutenção

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from app import app
 import os
 
 if __name__ == '__main__':
-    porta = int(os.getenv('PORT'), '5000')
-    app.run(host = '0.0.0.0', port = porta)
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host = '0.0.0.0', port = port)
 
