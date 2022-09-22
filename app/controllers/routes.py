e772ce0460a524abb678b2b7a665dfd19cda8dca
Airosa <airosa32@gmail.com>
2022-09-22 10:47:35 -0300

    atualização

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,12 +1,12 @@
+from email.policy import default
 from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    nome = 'Tiago'
-    dados = list(range(10))
-    return render_template('index.html',nome = nome, contador = dados)
+    lista = list(range(10))
+    return render_template('index.html', nome = 'tiago', lista = lista)
 
 @app.route('/contato')
 def contato():
